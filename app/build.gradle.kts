a74bcfe901529e8f5a189cf1fb59e6a4c866dfd5
marukuru <marukuru@gmx.net>
2026-08-13 19:27:38 +0200

    GitHub workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,8 +84,6 @@ android {
 kotlin {
   compilerOptions {
     jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-    languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
-    apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
   }
 }
 
